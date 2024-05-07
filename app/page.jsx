a0e2ae8ef047b29b1a85ca19db0d1a2b88692012
Aidan Douglas <itsquokka@gmail.com>
2024-05-07 16:29:25 -0400

    Updated Google Analytics

--- app/page.jsx
+++ app/page.jsx
@@ -4,18 +4,7 @@ import Testimonial from "@/components/Testimonial";
 import Why from "@/components/Why";
 import Image from "next/image";
 
-import { useLocation } from "react-router-dom";
-import { useEffect } from "react";
-
-import ReactGA from "react-ga";
-
-ReactGA.initialize("G-XZ495Z9CZK");
-
 export default function Home() {
-  const location = useLocation();
-  useEffect(() => {
-    ReactGA.pageview(location.pathname + location.search);
-  }, [location]);
   return (
     <main>
       <Hero />
